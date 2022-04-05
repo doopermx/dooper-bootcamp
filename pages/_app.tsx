e42b64d6da14a489de0cc6da227c71fae1c4c06b
Justino <tinotr95@gmail.com>
2022-04-05 11:19:24 -0500

    Forked repo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,11 +24,9 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-
-        
         <Component {...pageProps} />
 
-
+        
       </ThemeProvider>
     </CacheProvider>
   );
