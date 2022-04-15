217d265d0e4c94a1589b2a949d4d6d7c39b68fd5
stobar93 <sebastiantobarquintero@gmail.com>
2022-04-15 01:09:49 -0500

    Create Login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import useFormConfig from "@src/hooks/useFormConfig";
+import { FieldProps } from "@src/hooks/useFormConfig/useFormConfig";
+import TextInput from "../TextInput";
+import { Styledform } from "./styles";
+
+const fields: FieldProps[] = [
+  {
+    id: "email",
+    initialValue: "",
+    placeholder: "",
+    label: "Email",
+    validate: "email"
+  },
+  {
+    id: "password",
+    initialValue: "",
+    placeholder: "",
+    label: "Password",
+    validate: "password"
+  }
+];
+
+function Login() {
+  const formik = useFormConfig(fields);
+
+  return (
+    <Styledform onSubmit={formik.handleSubmit}>
+      {fields.map((field) => (
+        <TextInput key={field.id} field={field} formik={formik} />
+      ))}
+      {!formik.dirty || !formik.isValid ? (
+        <Button variant="outlined" disabled>
+          Submit
+        </Button>
+      ) : (
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      )}
+    </Styledform>
+  );
+}
+
+export default Login;
