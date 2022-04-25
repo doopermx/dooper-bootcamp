f65264121688620244fb50ae8463870ec12f51d0
stobar93 <sebastiantobarquintero@gmail.com>
2022-04-24 21:57:07 -0500

    Display published products using react-query and supabse auth helpers

---
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProductCard from "src/components/ProductsGrid/components/ProductCard";
+import { Box, Dialog, DialogTitle } from "@mui/material";
+import useProducts from "@src/hooks/useProducts";
+
+function ProductsGrid() {
+  const { products, isLoading } = useProducts();
+
+  return (
+    <Box display="flex" flexWrap="wrap" justifyContent="center">
+      {products?.map((product) => (
+        <ProductCard key={`product-${product.id}`} product={product} />
+      ))}
+      {
+        <Dialog open={isLoading}>
+          <DialogTitle id="alert-dialog-title">
+            {isLoading && "Loading..."}
+          </DialogTitle>
+        </Dialog>
+      }
+    </Box>
+  );
+}
+
+export default ProductsGrid;
